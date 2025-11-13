7949275c7380e671ae364d8b9d79da931d1bbfd3
Nicolas O <noccis@I7413>
2025-11-13 09:32:29 +0100

    Fix bug: propose to save buffer before evaluation only if .el file

--- init.el
+++ init.el
@@ -5926,11 +5926,12 @@ With Electric Indent Mode enabled, inserts a newline and indents
  (defun my-save-buffer-if-modified (&rest _args)
    "Propose to save the current buffer if it has been modified."
    (interactive)
-   (when (eq (buffer-local-value 'major-mode (current-buffer)) 'emacs-lisp-mode)
-     (when (buffer-modified-p)
-       (if (y-or-n-p (format "Emacs Lisp buffer %s modified; save it? " (buffer-name)))
-           (save-buffer)
-         (message "Buffer not saved")))))
+   (when (and (buffer-file-name)
+             (string-suffix-p ".el" (buffer-file-name))
+             (buffer-modified-p))
+     (if (y-or-n-p (format "Emacs Lisp buffer %s modified; save it? " (buffer-name)))
+         (save-buffer)
+       (message "Buffer not saved"))))
 
  (advice-add 'eval-buffer :before #'my-save-buffer-if-modified)
  ;; to have similar behaviour with C-c C-k for Common Lisp
